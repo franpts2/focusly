604da30f1de0c7b03df7678ce14a74e2c89179b6
Maria luiza <up202304306@up.pt>
2025-03-17 15:04:39 +0000

    created Profile page

---
+++ focusly/lib/view/profile/profile_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ProfileView extends StatelessWidget {
+  const ProfileView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Profile"), centerTitle: true),
+      body: Center(child: Text("to be implemented!")),
+    );
+  }
+}
