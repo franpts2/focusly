ad0f05e39de27fed671fcf8eef6d2a91054f37ec
Maria luiza <up202304306@up.pt>
2025-03-17 14:59:31 +0000

    created Create page

---
+++ focusly/lib/view/create/create_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class CreateView extends StatelessWidget {
+  const CreateView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Create"), centerTitle: true),
+      body: Center(child: Text("to be implemented!"),),
+    );
+  }
+}
