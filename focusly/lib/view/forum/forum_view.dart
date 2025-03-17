2200126acec54f7d22ad89a4be2d2def8d4b2220
Maria luiza <up202304306@up.pt>
2025-03-17 14:57:30 +0000

    created Forum page

---
+++ focusly/lib/view/forum/forum_view.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class ForumView extends StatelessWidget {
+  const ForumView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Forum"), centerTitle: true),
+      body: Center(child: Text("to be implemented!")),
+    );
+  }
+}
