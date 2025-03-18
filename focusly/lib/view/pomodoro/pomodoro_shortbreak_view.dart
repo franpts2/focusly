d91d9ac358ffd894c2f99b96e4569878a85b14c4
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-03-18 15:10:25 +0000

    Added the start buttons to each pomodoro mode

--- focusly/lib/view/pomodoro/pomodoro_shortbreak_view.dart
+++ focusly/lib/view/pomodoro/pomodoro_shortbreak_view.dart
@@ -10,10 +10,27 @@ class PomodoroShortbreakView extends StatefulWidget {
 class _PomodoroShortbreakViewState extends State<PomodoroShortbreakView> {
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
     return Center(
-      child: Text(  // temporary placeholder for the actual timer
-        "05:00",
-        style: TextStyle(fontSize: 57, fontWeight: FontWeight.w400),
+      child: Column(
+        children: [
+          Text(
+            // temporary placeholder for the actual timer
+            "05:00",
+            style: TextStyle(fontSize: 57, fontWeight: FontWeight.w400),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Start the timer
+            },
+            style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all(colorScheme.primary),
+              foregroundColor: WidgetStateProperty.all(colorScheme.onPrimary),
+            ),
+            child: Text("START"),
+          ),
+        ],
       ),
     );
   }
