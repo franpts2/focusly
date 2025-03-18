68920c87a7dfd422ba4cdceb463a25c15d8b5a09
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-03-18 15:10:25 +0000

    Added the start buttons to each pomodoro mode

--- focusly/lib/view/pomodoro/pomodoro_longbreak_view.dart
+++ focusly/lib/view/pomodoro/pomodoro_longbreak_view.dart
@@ -10,10 +10,27 @@ class PomodoroLongbreakView extends StatefulWidget {
 class _PomodoroLongbreakViewState extends State<PomodoroLongbreakView> {
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
     return Center(
-      child: Text(  // temporary placeholder for the actual timer
-        "15:00",
-        style: TextStyle(fontSize: 57, fontWeight: FontWeight.w400),
+      child: Column(
+        children: [
+          Text(
+            // temporary placeholder for the actual timer
+            "15:00",
+            style: TextStyle(fontSize: 57, fontWeight: FontWeight.w400),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Start the timer
+            },
+            style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all(colorScheme.primary),
+              foregroundColor: WidgetStateProperty.all(colorScheme.onPrimary),
+            ),
+            child: Text("START"),
+          ),
+        ],
       ),
     );
   }
--- focusly/lib/view/pomodoro/pomodoro_normal_view.dart
+++ focusly/lib/view/pomodoro/pomodoro_normal_view.dart
@@ -10,11 +10,57 @@ class PomodoroNormalView extends StatefulWidget {
 class _PomodoroNormalViewState extends State<PomodoroNormalView> {
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
+
     return Center(
-      child: Text(  // temporary placeholder for the actual timer
-        "25:00",
-        style: TextStyle(fontSize: 57, fontWeight: FontWeight.w400),
+      child: Column(
+        children: [
+          Text(
+            // temporary placeholder for the actual timer
+            "25:00",
+            style: TextStyle(fontSize: 57, fontWeight: FontWeight.w400),
+          ),
+          ElevatedButton(
+            onPressed: () {
+              // Start the timer
+            },
+            style: ButtonStyle(
+              backgroundColor: WidgetStateProperty.all(colorScheme.primary),
+              foregroundColor: WidgetStateProperty.all(colorScheme.onPrimary),
+            ),
+            child: Text("START"),
+          ),
+        ],
       ),
     );
   }
 }
+
+/* 
+-> The following buttons are meant to be associated with the respective funcionality
+
+PAUSE BUTTON: when the timer starts, this button replaces the start button and makes the timer pause
+ElevatedButton(
+  onPressed: () {
+    // Pause the timer
+  },
+  style: ButtonStyle(
+    backgroundColor: WidgetStateProperty.all(colorScheme.primary),
+    foregroundColor: WidgetStateProperty.all(colorScheme.onPrimary),
+  ),
+  child: Text("PAUSE"),
+),
+
+RESTART BUTTON: when the timer ends, this button replaces the pause button and makes the timer reset
+ElevatedButton(
+  onPressed: () {
+    // Reset the timer
+  },
+  style: ButtonStyle(
+    backgroundColor: WidgetStateProperty.all(colorScheme.primary),
+    foregroundColor: WidgetStateProperty.all(colorScheme.onPrimary),
+  ),
+  child: Text("RESET"),
+),
+
+*/
