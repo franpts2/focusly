20295c46fca85181c354372530aac3517673cf89
Maria luiza <up202304306@up.pt>
2025-03-26 19:50:02 +0000

    added Sign In with Google

--- focusly/lib/main.dart
+++ focusly/lib/main.dart
@@ -1,9 +1,23 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:focusly/view/navigation/navigation_view.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:provider/provider.dart';
+import 'package:focusly/auth/signin_view.dart';
+import 'package:focusly/services/authentication_service.dart';
 
-
-void main() {
-  runApp(const MainApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(create: (_) => AuthenticationService()),
+        // add other providers here if needed
+      ],
+      child: const MainApp(),
+    ),
+  );
 }
 
 class MainApp extends StatelessWidget {
@@ -11,6 +25,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(home: NavigationView());
+    return MaterialApp(
+      home: StreamBuilder<User?>(
+        stream: FirebaseAuth.instance.authStateChanges(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting ||
+              snapshot.connectionState == ConnectionState.none) {
+            return CircularProgressIndicator();
+          } else if (snapshot.hasData) {
+            return NavigationView();
+          } else {
+            return SignInView();
+          }
+        },
+      ),
+    );
   }
 }
