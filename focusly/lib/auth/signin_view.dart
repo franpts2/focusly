54107e6756fff3cf383f3bdcfc23116338a20d27
Maria luiza <up202304306@up.pt>
2025-03-26 19:50:02 +0000

    added Sign In with Google

--- focusly/lib/auth/signin_view.dart
+++ focusly/lib/auth/signin_view.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:focusly/services/authentication_service.dart';
+import 'package:provider/provider.dart';
 
 class SignInView extends StatelessWidget {
   const SignInView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            final authService = Provider.of<AuthenticationService>(context, listen: false,);
+            await authService.signIn();
+          },
+          child: const Text('Sign in with Google'),
+        ),
+      ),
+    );
   }
 }
