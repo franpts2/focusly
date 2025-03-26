3e993025e1032f36672e973270fe341db4b37cf2
gab1demattos <gab1demattos@yahoo.com>
2025-03-26 21:15:00 +0000

    added boxes in add section

--- focusly/lib/view/create/create_view.dart
+++ focusly/lib/view/create/create_view.dart
@@ -1,13 +1,56 @@
 import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/symbols.dart';
 
 class CreateView extends StatelessWidget {
   const CreateView({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final colorScheme = Theme.of(context).colorScheme;
     return Scaffold(
       appBar: AppBar(title: Text("Create"), centerTitle: true),
-      body: Center(child: Text("to be implemented!"),),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                const Icon(Symbols.add_circle), // Your icon
+                Text(' Add', style: TextStyle(fontSize: 22)),
+              ]
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Flexible(
+                  child: Container(
+                    margin: const EdgeInsets.all(10.0),
+                    width: 170,
+                    height: 170,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      color: colorScheme.primaryContainer,
+                    )
+                  )
+                ),
+                Flexible(
+                    child: Container(
+                        margin: const EdgeInsets.all(10.0),
+                        width: 170,
+                        height: 170,
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(12),
+                          color: colorScheme.primaryContainer,
+                        )
+                    )
+                ),
+              ],
+            )
+          ],
+        ),
+      )
     );
   }
 }
