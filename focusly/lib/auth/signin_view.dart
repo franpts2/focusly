d03aadb89f1961a8bf3b1c0cebd06e8e26109783
Maria luiza <up202304306@up.pt>
2025-03-27 17:43:01 +0000

    added 'Welcome Back' text in SignInView

--- focusly/lib/auth/signin_view.dart
+++ focusly/lib/auth/signin_view.dart
@@ -7,15 +7,58 @@ class SignInView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final primaryColor =
+        Theme.of(context).primaryColor; // Get the primary color from the theme
+
     return Scaffold(
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () async {
-            final authService = Provider.of<AuthenticationService>(context, listen: false,);
-            await authService.signIn();
-          },
-          child: const Text('Sign in with Google'),
-        ),
+      body: Column(
+        crossAxisAlignment:
+            CrossAxisAlignment.start, // Align children to the start (left)
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(
+              top: 250,
+              left: 20,
+            ), // Adjust position of the text
+            child: RichText(
+              textAlign: TextAlign.left, // Align text to the left
+              text: TextSpan(
+                children: [
+                  TextSpan(
+                    text: "Welcome\n",
+                    style: TextStyle(
+                      fontSize: 57,
+                      fontWeight: FontWeight.w400,
+                      color: primaryColor, // Use the primary color
+                    ),
+                  ),
+                  TextSpan(
+                    text: "Back",
+                    style: TextStyle(
+                      fontSize: 57,
+                      fontWeight: FontWeight.w400,
+                      color: primaryColor, // Use the primary color
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Spacer(), // Pushes the button to the bottom
+          Center(
+            child: ElevatedButton(
+              onPressed: () async {
+                final authService = Provider.of<AuthenticationService>(
+                  context,
+                  listen: false,
+                );
+                await authService.signIn();
+              },
+              child: const Text('Sign in with Google'),
+            ),
+          ),
+          const SizedBox(height: 50), // Add spacing at the bottom
+        ],
       ),
     );
   }
