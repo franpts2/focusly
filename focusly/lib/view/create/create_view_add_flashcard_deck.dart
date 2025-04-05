fbc256b0204b494e5a77ed7d0f2fa348e836f489
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-04-05 13:10:46 +0100

    Implemented the flashcard add logic

--- focusly/lib/view/create/create_view_add_flashcard_deck.dart
+++ focusly/lib/view/create/create_view_add_flashcard_deck.dart
@@ -1,17 +1,69 @@
 import 'package:flutter/material.dart';
 import 'package:material_symbols_icons/symbols.dart';
 
-class CreateViewAddFlashcard extends StatefulWidget {
-  const CreateViewAddFlashcard({super.key});
+class CreateViewAddFlashcardDeck extends StatefulWidget {
+  const CreateViewAddFlashcardDeck({super.key});
 
   @override
-  State<CreateViewAddFlashcard> createState() => _CreateViewAddFlashcardState();
+  State<CreateViewAddFlashcardDeck> createState() => _CreateViewAddFlashcardState();
 }
 
-class _CreateViewAddFlashcardState extends State<CreateViewAddFlashcard> {
+class _CreateViewAddFlashcardState extends State<CreateViewAddFlashcardDeck> {
   final _titleController = TextEditingController();
   final _categoryController = TextEditingController();
   final List<Flashcard> _flashcards = [];
+
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _categoryController.dispose();
+    super.dispose();
+  }
+
+  void _addFlashcard() {
+    // Show edit dialog for a new flashcard
+    _showFlashcardDialog(
+      flashcard: Flashcard(front: '', back: ''),
+      isNew: true,
+    );
+  }
+
+  void _showFlashcardDialog({
+    required Flashcard flashcard,
+    bool isNew = false,
+  }) {
+    showDialog(
+      context: context,
+      builder: (context) => FlashcardEditDialog(
+        flashcard: flashcard,
+        onSave: (updatedFlashcard) {
+          setState(() {
+            if (isNew) {
+              _flashcards.add(updatedFlashcard);
+            } else {
+              // Update existing flashcard
+              final index = _flashcards.indexWhere(
+                (f) => f.front == flashcard.front && f.back == flashcard.back
+              );
+              if (index != -1) {
+                _flashcards[index] = updatedFlashcard;
+              }
+            }
+          });
+        },
+        onDelete: isNew
+          ? null // No delete for new flashcards
+          : () {
+              setState(() {
+                _flashcards.removeWhere(
+                  (f) => f.front == flashcard.front && f.back == flashcard.back
+                );
+              });
+              Navigator.pop(context);
+            },
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -67,7 +119,7 @@ class _CreateViewAddFlashcardState extends State<CreateViewAddFlashcard> {
                   style: Theme.of(context).textTheme.titleMedium,
                 ),
                 IconButton(
-                  onPressed: (){},
+                  onPressed: _addFlashcard,
                   icon: const Icon(Symbols.add_circle),
                 ),
               ],
@@ -120,6 +172,85 @@ class _CreateViewAddFlashcardState extends State<CreateViewAddFlashcard> {
           },
         ),
       ),
+    );
+  }
+}
+
+class FlashcardEditDialog extends StatefulWidget {
+  final Flashcard flashcard;
+  final Function(Flashcard) onSave;
+  final VoidCallback? onDelete;
+
+  const FlashcardEditDialog({
+    super.key,
+    required this.flashcard,
+    required this.onSave,
+    this.onDelete,
+  });
+
+  @override
+  State<FlashcardEditDialog> createState() => _FlashcardEditDialogState();
+}
+
+class _FlashcardEditDialogState extends State<FlashcardEditDialog> {
+  late TextEditingController _frontController;
+  late TextEditingController _backController;
+
+  @override
+  void initState() {
+    super.initState();
+    _frontController = TextEditingController(text: widget.flashcard.front);
+    _backController = TextEditingController(text: widget.flashcard.back);
+  }
+
+  @override
+  void dispose() {
+    _frontController.dispose();
+    _backController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: const Text('Edit Flashcard'),
+      content: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextField(
+            controller: _frontController,
+            decoration: const InputDecoration(
+              labelText: 'Front',
+              border: OutlineInputBorder(),
+            ),
+          ),
+          const SizedBox(height: 16),
+          TextField(
+            controller: _backController,
+            decoration: const InputDecoration(
+              labelText: 'Back',
+              border: OutlineInputBorder(),
+            ),
+          ),
+        ],
+      ),
+      actions: [
+        if (widget.onDelete != null)
+          TextButton(
+            onPressed: widget.onDelete,
+            child: const Text('DELETE'),
+          ),
+        TextButton(
+          onPressed: () {
+            widget.onSave(Flashcard(
+              front: _frontController.text,
+              back: _backController.text,
+            ));
+            Navigator.pop(context);
+          },
+          child: const Text('SAVE'),
+        ),
+      ],
     );
   }
 }
