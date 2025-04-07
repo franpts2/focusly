f29695697215647f342354d621813995a04738a0
Francisca Portugal <159019602+franpts2@users.noreply.github.com>
2025-04-07 10:57:20 +0100

    Added the layout of flashcard display

---
+++ focusly/lib/view/home/flashcard_deck_view.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+
+class FlashcardDeckView extends StatefulWidget {
+  const FlashcardDeckView({super.key});
+
+  @override
+  State<FlashcardDeckView> createState() => _FlashcardDeckViewState();
+}
+
+class _FlashcardDeckViewState extends State<FlashcardDeckView> {
+  @override
+  Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+
+    final cardWidth = screenWidth * 0.8; // 80% of screen width
+    final cardHeight = cardWidth / (345 / 246);
+    final colorScheme = Theme.of(context).colorScheme;
+
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Deck Name'),
+        centerTitle: true,
+        leading: IconButton(icon: Icon(Icons.arrow_back), onPressed: () {}),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            SizedBox(
+              width: cardWidth,
+              height: cardHeight,
+              child: Card(
+                elevation: 2,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(12.0),
+                ),
+                color: colorScheme.surfaceContainer,
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Text(
+                      'Example',
+                      style: TextStyle(fontSize: 24),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.symmetric(
+                horizontal: (screenWidth - cardWidth) / 2,
+                vertical: 20.0,
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  IconButton.filled(
+                    icon: Icon(Icons.arrow_back, color: colorScheme.onPrimary),
+                    onPressed: () {
+                      // Handle left arrow button press
+                    },
+                  ),
+                  IconButton.filled(
+                    icon: Icon(
+                      Icons.arrow_forward,
+                      color: colorScheme.onPrimary,
+                    ),
+                    onPressed: () {
+                      // Handle right arrow button press
+                    },
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
