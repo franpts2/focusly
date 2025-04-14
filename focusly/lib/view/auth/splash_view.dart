eda310df4507ee29bb5cc3e46875bd3a511d5e1c
Maria luiza <up202304306@up.pt>
2025-04-14 15:52:30 +0100

    added splash screen (still deciding the transition)

---
+++ focusly/lib/view/auth/splash_view.dart
@@ -0,0 +1,71 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:focusly/view/auth/initial_view.dart';
+
+class SplashView extends StatefulWidget {
+  const SplashView({super.key});
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  void initState() {
+    super.initState();
+    // Navigate to the initial view after a delay
+    // Future.delayed(const Duration(seconds: 3), () {
+    //   Navigator.of(context).pushReplacement(
+    //     MaterialPageRoute(builder: (context) => const InitialPageView()),
+    //   );
+    // });
+
+    Timer(Duration(seconds: 3), () {
+      Navigator.pushReplacement(
+        context,
+        PageRouteBuilder(
+          transitionDuration: Duration(milliseconds: 800),
+          pageBuilder: (_, __, ___) => InitialPageView(),
+          transitionsBuilder: (_, animation, __, child) {
+            return FadeTransition(
+              opacity: animation,
+              child: ScaleTransition(
+                scale: Tween<double>(begin: 0.95, end: 1.0).animate(animation),
+                child: child,
+              ),
+            );
+          },
+        ),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Theme.of(context).primaryColor,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CircleAvatar(
+              radius: 80,
+              backgroundColor: Colors.white,
+              child: Image.asset('assets/icon/elephant_logo.png', width: 100),
+            ),
+            const SizedBox(height: 24),
+            const Text(
+              'focusly',
+              style: TextStyle(
+                fontSize: 32,
+                fontWeight: FontWeight.bold,
+                color: Colors.white,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
